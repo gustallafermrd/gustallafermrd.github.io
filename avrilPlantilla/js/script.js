7136871f5dec94b7ada3b276a248cc66216684b3
Gustavo Tallaferro <36135271+gustallafermrd@users.noreply.github.com>
2020-07-31 16:27:12 -0300

    Bug Fixes

--- avrilPlantilla/js/script.js
+++ avrilPlantilla/js/script.js
@@ -338,20 +338,4 @@ $(window).scroll(function() {
 		delay: 10,
 		time: 1000
 	});
-
-	// Shuffle js filter and masonry
-	var Shuffle = window.Shuffle;
-	var jQuery = window.jQuery;
-
-	var myShuffle = new Shuffle(document.querySelector(".shuffle-wrapper"), {
-		itemSelector: ".shuffle-item",
-		buffer: 1
-	});
-
-	jQuery('input[name="shuffle-filter"]').on("change", function(evt) {
-		var input = evt.currentTarget;
-		if (input.checked) {
-			myShuffle.filter(input.value);
-		}
-	});
 })(jQuery);
